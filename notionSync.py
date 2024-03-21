d9e4f18a42e96570b4d9fe360797903867c6c7c0
Omar MELLOULCHI <omar.melloulchi@greenwishes.fr>
2024-03-21 15:56:03 +0100

    Update NotionSync script
    Add a check for duplicate entries. Also if duplicate, delete function can be used. The call has been commented out.

--- notionSync.py
+++ notionSync.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 import os
 from datetime import datetime
 
-
 load_dotenv()
 
 github_token = os.getenv('GITHUB_TOKEN')
@@ -18,29 +17,6 @@ repo_name = os.getenv('REPO_NAME')
 # end_date = datetime.strptime(end_date_str, "%Y-%m-%d")
 
 # TODO: Add a Commit ID Field to Notion Database, Modify the Script to Use Another Unique Identifier
-
-# def query_commit_in_notion(notion_token, database_id, commit_id):
-#     query_url = f"https://api.notion.com/v1/databases/{database_id}/query"
-#     headers = {
-#         "Authorization": f"Bearer {notion_token}",
-#         "Content-Type": "application/json",
-#         "Notion-Version": "2022-06-28"
-#     }
-#     query_data = {
-#         "filter": {
-#             "property": "Commit ID",
-#             "text": {
-#                 "equals": commit_id
-#             }
-#         }
-#     }
-#     response = requests.post(query_url, headers=headers, json=query_data)
-#     if response.status_code == 200:
-#         results = response.json().get("results", [])
-#         return len(results) > 0
-#     else:
-#         print(f"Error querying commit in Notion: {response.status_code}")
-#         return False
 
 def fetch_org_repos(org_name, github_token):
     github_api_url = f"https://api.github.com/orgs/{org_name}/repos"
@@ -64,7 +40,20 @@ def fetch_commits_for_user_in_repo(github_token, org_name, repo_name):
             print(f"Aucun commit trouvé pour {repo}.")
             return []
 
+def delete_duplicate_commits(notion_token, block_id):
+    headers = {
+        "Authorization": f"Bearer {notion_token}",
+        "Notion-Version": "2022-06-28",
+    }
+    delete_url = f"https://api.notion.com/v1/blocks/{block_id}"  # URL corrigée
+    response = requests.delete(delete_url, headers=headers)
+    if response.status_code == 200:
+        print(f"Deleted duplicate commit block successfully: {block_id}")
+    else:
+        print(f"Failed to delete duplicate commit block: {block_id}, Status Code: {response.status_code}, Response: {response.text}")
+
 def update_notion(notion_token, database_id, commits, repo_name):
+    notion_query_url = f"https://api.notion.com/v1/databases/{database_id}/query"
     notion_api_url = "https://api.notion.com/v1/pages"
     headers = {
         "Authorization": f"Bearer {notion_token}",
@@ -72,24 +61,54 @@ def update_notion(notion_token, database_id, commits, repo_name):
         "Notion-Version": "2022-06-28"
     }
     for commit in commits:
-        # commit_sha = commit['sha']
-        # if not query_commit_in_notion(notion_token, database_id, commit_sha):
-            commit_message = commit.get("commit", {}).get("message", "No commit message")
-            data = {
-                "parent": {"database_id": database_id},
-                "properties": {
-                    "Name": {"title": [{"text": {"content": commit_message.split('\n')[0]}}]},
-                    "Date": {"date": {"start": commit["commit"]["author"]["date"]}},
-                    "Repository": {"rich_text": [{"text": {"content": repo_name}}]}
+        commit_message_first_line = commit.get("commit", {}).get("message", "No commit message").split('\n')[0]
+        commit_date = commit.get("commit", {}).get("author", {}).get("date", "")
+        query_data = {
+            "filter": {
+                "and": [
+                    {
+                        "property": "Name",
+                        "title": {
+                            "equals": commit_message_first_line
+                        }
+                    },
+                    {
+                        "property": "Repository",
+                        "rich_text": {
+                            "equals": repo_name
+                        }
+                    }
+                ]
+            }
+        }
+        response = requests.post(notion_query_url, headers=headers, json=query_data)
+
+        if response.status_code == 200 and response.json().get("results"):
+            page_ids = [page['id'] for page in response.json()["results"]]
+            print(f"Commit already exists in Notion, skipping and attempting to delete duplicates: {commit_message_first_line}")
+            for page_id in page_ids:
+                print(f"Attempting to delete page with ID: {page_id}")
+                # delete_duplicate_commits(notion_token, page_id)
+            continue
+        data = {
+            "parent": {"database_id": database_id},
+            "properties": {
+                "Name": {
+                    "title": [{"text": {"content": commit_message_first_line}}]
+                },
+                "Date": {
+                    "date": {"start": commit.get("commit", {}).get("author", {}).get("date", "")}
+                },
+                "Repository": {
+                    "rich_text": [{"text": {"content": repo_name}}]
                 }
             }
-            response = requests.post(notion_api_url, headers=headers, json=data)
-            if response.status_code == 200:
-                print(f"Commit '{commit_sha}' added to Notion successfully.")
-            else:
-                print(f"Error adding commit to Notion: {response.status_code}, Response: {response.text}")
-        # else:
-        #     print(f"Commit '{commit_sha}' already exists in Notion. Skipping.")
+        }
+        response = requests.post(notion_api_url, headers=headers, json=data)
+        if response.status_code == 200:
+            print(f"Commit added to Notion successfully: {commit_message_first_line}")
+        else:
+            print(f"Error adding commit to Notion: {response.status_code}, Response: {response.text}")
 
 def main(github_token, org_name, repo_name):
     commits = fetch_commits_for_user_in_repo(github_token, org_name, repo_name)
@@ -100,7 +119,6 @@ def main(github_token, org_name, repo_name):
         update_notion(notion_token, database_id, commits, repo_name)
     else:
         print(f"Aucun commit trouvé dans le dépôt {repo_name}.")
-
 
 if __name__ == "__main__":
     main(github_token, org_name, repo_name)
