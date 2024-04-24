21f43cfc504932432ee196f9c200c0e825c668ce
Omar MELLOULCHI <omar.melloulchi@greenwishes.fr>
2024-04-24 16:05:11 +0200

    Add NotionSync instance call to index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const NotionSync = require('./notionSync');
+require('dotenv').config();
+
+
+const app = async () => {
+    const githubToken = process.env.GITHUB_TOKEN;
+    const notionToken = process.env.NOTION_TOKEN;
+    const databaseId = process.env.NOTION_DATABASE_ID;
+    const orgName = process.env.ORG_NAME;
+    const repoName = process.env.REPO_NAME;
+    const mistralToken = process.env.MISTRAL_TOKEN;
+    const notionSync = new NotionSync(githubToken, notionToken, databaseId, orgName, repoName, mistralToken);
+    await notionSync.sync();
+    console.log('Sync complete 🚀');
+}
+
+app();
