9e1ace1c354e5729143dc0ccb9c4252fee054f32
Omar MELLOULCHI <omar.melloulchi@greenwishes.fr>
2024-05-28 22:18:54 +0200

    chore: Add next.config.js for proxying API requests

---
+++ web/next.config.js
@@ -0,0 +1,17 @@
+const withTM = require('next-transpile-modules')(['lucide-react']); // Include your package here
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:3000/:path*", // Proxy to Backend
+      },
+    ];
+  },
+};
+
+module.exports = withTM(nextConfig);
