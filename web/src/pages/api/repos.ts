c9a53faf0e1ec5847c209d4246d9302bd10a099a
Omar MELLOULCHI <omar.melloulchi@greenwishes.fr>
2024-05-31 23:06:32 +0200

    feat: Update repos API endpoint to fetch branches for each repository

--- web/src/pages/api/repos.ts
+++ web/src/pages/api/repos.ts
@@ -1,20 +1,26 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { NotionSync } from "../../../../api/src/notionSync";
 
-type ReposResponse = {
+type Response = {
   repos?: string[];
+  branches?: { [repo: string]: string[] };
   error?: string;
 };
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<ReposResponse>
+  res: NextApiResponse<Response>
 ) {
   if (req.method === "GET") {
-    const { username } = req.query;
+    const { username, githubToken } = req.query;
 
     if (!username || typeof username !== "string") {
       res.status(400).json({ error: "Invalid or missing username" });
+      return;
+    }
+
+    if (!githubToken || typeof githubToken !== "string") {
+      res.status(400).json({ error: "Invalid or missing GitHub token" });
       return;
     }
 
@@ -22,7 +28,17 @@ export default async function handler(
 
     try {
       const repos = await notionSync.fetchUserRepos(username);
-      res.status(200).json({ repos });
+      const branches: { [repo: string]: string[] } = {};
+
+      for (const repo of repos) {
+        const branchList = await notionSync.fetchRepoBranches(
+          username,
+          repo,
+          githubToken
+        );
+        branches[repo] = branchList;
+      }
+      res.status(200).json({ repos, branches });
     } catch (error) {
       const errorMessage = (error as Error).message;
       res.status(500).json({ error: errorMessage });
