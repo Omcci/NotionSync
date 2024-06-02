4da13288d5957915da2f0c251211fcb12d9c8211
Omar MELLOULCHI <omar.melloulchi@greenwishes.fr>
2024-06-02 20:15:40 +0200

    refactor: remove func to fetch branches for the moment

--- web/src/pages/api/repos.ts
+++ web/src/pages/api/repos.ts
@@ -1,50 +1,34 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { NotionSync } from "../../../../api/src/notionSync";
 
-type Response = {
+const notionSync = new NotionSync();
+
+type ReposResponse = {
   repos?: string[];
-  branches?: { [repo: string]: string[] };
   error?: string;
 };
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Response>
+  res: NextApiResponse<ReposResponse>
 ) {
-  if (req.method === "GET") {
-    const { username, githubToken } = req.query;
-
-    if (!username || typeof username !== "string") {
-      res.status(400).json({ error: "Invalid or missing username" });
-      return;
-    }
-
-    if (!githubToken || typeof githubToken !== "string") {
-      res.status(400).json({ error: "Invalid or missing GitHub token" });
-      return;
-    }
-
-    const notionSync = new NotionSync();
-
-    try {
-      const repos = await notionSync.fetchUserRepos(username);
-      const branches: { [repo: string]: string[] } = {};
-
-      for (const repo of repos) {
-        const branchList = await notionSync.fetchRepoBranches(
-          username,
-          repo,
-          githubToken
-        );
-        branches[repo] = branchList;
-      }
-      res.status(200).json({ repos, branches });
-    } catch (error) {
-      const errorMessage = (error as Error).message;
-      res.status(500).json({ error: errorMessage });
-    }
-  } else {
+  if (req.method !== "GET") {
     res.setHeader("Allow", ["GET"]);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+  const { username } = req.query;
+
+  if (!username || typeof username !== "string") {
+    return res.status(400).json({ error: "Invalid or missing username" });
+  }
+
+  try {
+    const repos = await notionSync.fetchUserRepos(username);
+    console.log("Fetched repos:", repos);
+
+    res.status(200).json({ repos });
+  } catch (error) {
+    const errorMessage = (error as Error).message;
+    res.status(500).json({ error: errorMessage });
   }
 }
