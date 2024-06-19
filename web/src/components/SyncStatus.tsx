0d800325b05aaad68694ff6e636d5372783b60a7
Omar MELLOULCHI <omar.melloulchi@greenwishes.fr>
2024-06-19 12:17:07 +0200

    refactor: update return in case of error using ternary operator for null case

--- web/src/components/SyncStatus.tsx
+++ web/src/components/SyncStatus.tsx
@@ -29,22 +29,6 @@ const SyncStatus = () => {
     return () => clearInterval(interval);
   }, []);
 
-  if (!syncStatus) {
-    return (
-      <div className="bg-white dark:bg-gray-900 rounded-lg shadow p-6 mb-6">
-        <div className="flex items-center justify-between">
-          <div>
-            <h2 className="text-lg font-bold">Last Sync Status</h2>
-            <p className="text-gray-500 dark:text-gray-400">
-              Last successful sync: {""}
-              <span className="font-medium">Loading...</span>
-            </p>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
   return (
     <div className="bg-white dark:bg-gray-900 rounded-lg shadow p-6 mb-6">
       <div className="flex items-center justify-between">
@@ -52,14 +36,14 @@ const SyncStatus = () => {
           <h2 className="text-lg font-bold">Last Sync Status</h2>
           <p className="text-gray-500 dark:text-gray-400">
             Last successful sync: {""}
-            <span className="font-medium">{syncStatus.lastSyncDate}</span>
+            <span className="font-medium">{syncStatus ? syncStatus.lastSyncDate : "Loading ..."}</span>
           </p>
         </div>
-        {syncStatus.errorBranch && (
+        {syncStatus!.errorBranch && (
           <div>
             <p className="text-red-500 dark:text-red-400">
               <CircleAlertIcon className="w-5 h-5 mr-2 inline" />
-              {syncStatus.statusMessage}
+              {syncStatus!.statusMessage}
             </p>
           </div>
         )}
