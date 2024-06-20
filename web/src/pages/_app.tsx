d3eedd5a5a90e494ab20be1e1ce458eb2e40cfd6
Omar MELLOULCHI <omar.melloulchi@greenwishes.fr>
2024-06-20 22:34:04 +0200

    feat: Add ConfigProvider component for handling configuration settings
    This commit adds the ConfigProvider component to handle configuration settings in the application. It sets the initial configuration and fetches the configuration from the server. This update enhances the application by providing a centralized way to manage and access the configuration settings.

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -6,6 +6,7 @@ import "../../styles/globals.css";
 import { Inter } from "next/font/google";
 import Head from "next/head";
 import { AppProvider } from "@/context/AppContext";
+import { ConfigProvider } from "@/context/ConfigContext";
 
 const interFont = Inter({
   subsets: ["latin"],
@@ -15,18 +16,20 @@ const interFont = Inter({
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AppProvider>
-      <Layout>
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          <style dangerouslySetInnerHTML={{ __html: interFont.style }} />
-        </Head>
-        <div className={interFont.className}>
-          <Component {...pageProps} />
-        </div>
-      </Layout>
+      <ConfigProvider>
+        <Layout>
+          <Head>
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1.0"
+            />
+            <style dangerouslySetInnerHTML={{ __html: interFont.style }} />
+          </Head>
+          <div className={interFont.className}>
+            <Component {...pageProps} />
+          </div>
+        </Layout>
+      </ConfigProvider>
     </AppProvider>
   );
 }
