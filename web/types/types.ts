081dff282d5975adfeda7a9da9a6d5dc163205da
Omar MELLOULCHI <omar.melloulchi@greenwishes.fr>
2024-07-01 23:37:20 +0200

    feat: create types file and export types

---
+++ web/types/types.ts
@@ -0,0 +1,20 @@
+// Commit type
+export type Commit = {
+    commit: string;
+    branch: string;
+    author: string;
+    date: string;
+    status: string;
+    actions: string[];
+    avatar_url: string;
+  };
+  
+  // Config type
+  export type Config = {
+    githubToken: string;
+    notionToken: string;
+    repoName: string;
+    orgName: string;
+  };
+
+  
