39d60ecccdb8a3ff5441945a4e1d9df0b995c594
Omar MELLOULCHI <omar.melloulchi@greenwishes.fr>
2024-07-02 23:14:14 +0200

    refactor: Update ConfigSettings component to use Accordion component for collapsible settings

--- web/src/components/config-settings/ConfigSettings.tsx
+++ web/src/components/config-settings/ConfigSettings.tsx
@@ -1,20 +1,33 @@
 import { SettingsIcon } from '../../../public/icon/SettingsIcon'
 import { Button } from '../ui/button'
 import ConfigSettingsForm from './ConfigSettingsForm'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '../ui/accordion'
 
 const ConfigSettings = () => {
   return (
-    <div className="bg-white dark:bg-gray-900 rounded-lg shadow p-6 mb-6">
-      <div className="flex items-center justify-between mb-4">
-        <h2 className="text-lg font-bold">Configuration Settings</h2>
-        <div className="flex items-center gap-4">
-          <Button className="flex items-center gap-2" variant="outline">
-            <SettingsIcon className="w-5 h-5" />
-            Edit Settings
-          </Button>
-        </div>
-      </div>
-      <ConfigSettingsForm />
+    <div className="bg-white dark:bg-gray-900 rounded-lg shadow p-6 my-6">
+      <Accordion type="single" collapsible>
+        <AccordionItem value="settings">
+          <AccordionTrigger className="flex items-center justify-between mb-4">
+            <h2 className="text-lg font-bold">Configuration Settings</h2>
+          </AccordionTrigger>
+          <AccordionContent>
+            <div className="flex justify-end mb-2">
+              <Button className="flex items-center gap-2" variant="outline">
+                <SettingsIcon className="w-5 h-5" />
+                Edit Settings
+              </Button>
+            </div>
+
+            <ConfigSettingsForm />
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </div>
   )
 }
