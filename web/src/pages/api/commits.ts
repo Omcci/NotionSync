b36d2c34b52b55d8431fde7b023f0dcf16641e2e
Omar MELLOULCHI <omar.melloulchi@greenwishes.fr>
2024-07-02 23:35:18 +0200

    refactor: Update commits API to support pagination and custom page size

--- web/src/pages/api/commits.ts
+++ web/src/pages/api/commits.ts
@@ -1,15 +1,110 @@
+// import { NextApiRequest, NextApiResponse } from 'next'
+// import { Action, Commit } from '../../../types/types'
+
+// export default async (req: NextApiRequest, res: NextApiResponse) => {
+//   const { repoName, orgName } = req.query as {
+//     repoName: string
+//     orgName: string
+//   }
+
+//   console.log(`Fetching commits for ${orgName}/${repoName}`)
+
+//   const url = `https://api.github.com/repos/${orgName}/${repoName}/commits`
+//   const token = process.env.NEXT_PUBLIC_GITHUB_TOKEN
+
+//   console.log(`GitHub API URL: ${url}`)
+//   console.log(`GitHub Token: ${token ? 'Present' : 'Not Present'}`)
+
+//   try {
+//     let commits: Commit[] = []
+//     let page = 1
+//     let fetchMore = true
+
+//     while (fetchMore) {
+//       const response = await fetch(`${url}?per_page=100&page=${page}`, {
+//         headers: {
+//           Authorization: `token ${token}`,
+//         },
+//       })
+
+//       console.log(`Status: ${response.status}`)
+//       console.log(`Headers: ${JSON.stringify(response.headers)}`)
+
+//       const rawResponse = await response.text()
+
+//       if (!response.ok) {
+//         throw new Error(`Error fetching commits: ${response.status}`)
+//       }
+
+//       let pageCommits
+//       try {
+//         pageCommits = JSON.parse(rawResponse)
+//       } catch (parseError) {
+//         throw new Error(
+//           `Failed to parse JSON response: ${(parseError as Error).message}`,
+//         )
+//       }
+
+//       console.log(`Commits: ${JSON.stringify(pageCommits)}`)
+//       commits = [...commits, ...pageCommits]
+
+//       if (pageCommits.length < 100) {
+//         fetchMore = false
+//       } else {
+//         page++
+//       }
+//     }
+
+//     // const commits = await response.json()
+//     const formattedCommits = commits.map((commit: any) => {
+//       const status =
+//         commit.commit.verification && commit.commit.verification.verified
+//           ? 'Verified'
+//           : 'Unverified'
+
+//       return {
+//         commit: commit.commit.message,
+//         branch: commit.commit.tree.sha,
+//         author: commit.commit.author.name,
+//         date: commit.commit.author.date,
+//         status: status,
+//         actions: [
+//           { name: 'View', url: `${url}/${commit.sha}` },
+//           { name: 'Github', url: commit.html_url },
+//           // { name: 'Notebook', url: `http://yourapp.com/notebook/${commit.sha}` },
+//         ] as Action[],
+//         avatar_url: commit.committer
+//           ? commit.committer.avatar_url
+//           : 'https://github.com/identicons/default.png',
+//       }
+//     })
+
+//     res.status(200).json(formattedCommits)
+//   } catch (error: any) {
+//     console.error(error.message)
+//     res.status(500).json({ error: error.message })
+//   }
+// }
+
 import { NextApiRequest, NextApiResponse } from 'next'
 import { Action } from '../../../types/types'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { repoName, orgName } = req.query as {
+  const {
+    repoName,
+    orgName,
+    page = '1',
+    per_page = '10',
+  } = req.query as {
     repoName: string
     orgName: string
+    page: string
+    per_page: string
   }
 
   console.log(`Fetching commits for ${orgName}/${repoName}`)
 
-  const url = `https://api.github.com/repos/${orgName}/${repoName}/commits`
+  const url = `https://api.github.com/repos/${orgName}/${repoName}/commits?page=${page}&per_page=${per_page}`
   const token = process.env.NEXT_PUBLIC_GITHUB_TOKEN
 
   console.log(`GitHub API URL: ${url}`)
@@ -32,13 +127,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       throw new Error(`Error fetching commits: ${response.status}`)
     }
 
-    if (!response.ok) {
-      throw new Error(
-        `Error fetching commits: ${response.status} - ${rawResponse}`,
-      )
-    }
-
-    // Attempt to parse the response as JSON
     let commits
     try {
       commits = JSON.parse(rawResponse)
@@ -50,9 +138,11 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     console.log(`Commits: ${JSON.stringify(commits)}`)
 
-    // const commits = await response.json()
     const formattedCommits = commits.map((commit: any) => {
-      const status = commit.commit.verification && commit.commit.verification.verified ? 'Verified' : 'Unverified'
+      const status =
+        commit.commit.verification && commit.commit.verification.verified
+          ? 'Verified'
+          : 'Unverified'
 
       return {
         commit: commit.commit.message,
@@ -63,11 +153,10 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         actions: [
           { name: 'View', url: `${url}/${commit.sha}` },
           { name: 'Github', url: commit.html_url },
-          // { name: 'Notebook', url: `http://yourapp.com/notebook/${commit.sha}` },
         ] as Action[],
         avatar_url: commit.committer
           ? commit.committer.avatar_url
-          : 'https://github.com/identicons/default.png', // Include the avatar URL
+          : 'https://github.com/identicons/default.png',
       }
     })
 
