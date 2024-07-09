485020eed56d3e4877c7d07fd90e93ef654ce07b
Omar MELLOULCHI <omar.melloulchi@greenwishes.fr>
2024-07-09 14:58:57 +0200

    refactor: add export to repos.ts (fetchCommitsForUserInRepo)

--- web/src/pages/api/commits.ts
+++ web/src/pages/api/commits.ts
@@ -1,66 +1,79 @@
 // import { NextApiRequest, NextApiResponse } from 'next'
-// import { Action, Commit } from '../../../types/types'
+// import { Action } from '../../../types/types'
 
 // export default async (req: NextApiRequest, res: NextApiResponse) => {
-//   const { repoName, orgName } = req.query as {
+//   const {
+//     repoName,
+//     orgName,
+//     page = '1',
+//     per_page = '10',
+//   } = req.query as {
 //     repoName: string
 //     orgName: string
+//     page: string
+//     per_page: string
 //   }
 
 //   console.log(`Fetching commits for ${orgName}/${repoName}`)
 
-//   const url = `https://api.github.com/repos/${orgName}/${repoName}/commits`
-//   const token = process.env.NEXT_PUBLIC_GITHUB_TOKEN
+//   const commitsUrl = `https://api.github.com/repos/${orgName}/${repoName}/commits?page=${page}&per_page=${per_page}`
+//   const pullRequestsUrl = `https://api.github.com/repos/${orgName}/${repoName}/pulls?state=open`
+//   const token = process.env.GITHUB_TOKEN
 
-//   console.log(`GitHub API URL: ${url}`)
+//   console.log(`GitHub API URL: ${commitsUrl}`)
 //   console.log(`GitHub Token: ${token ? 'Present' : 'Not Present'}`)
 
 //   try {
-//     let commits: Commit[] = []
-//     let page = 1
-//     let fetchMore = true
-
-//     while (fetchMore) {
-//       const response = await fetch(`${url}?per_page=100&page=${page}`, {
+//     const [commitsResponse, pullRequestsResponse] = await Promise.all([
+//       fetch(commitsUrl, {
 //         headers: {
 //           Authorization: `token ${token}`,
 //         },
-//       })
+//       }),
+//       fetch(pullRequestsUrl, {
+//         headers: {
+//           Authorization: `token ${token}`,
+//         },
+//       }),
+//     ])
 
-//       console.log(`Status: ${response.status}`)
-//       console.log(`Headers: ${JSON.stringify(response.headers)}`)
+//     console.log(`Commits Status: ${commitsResponse.status}`)
+//     console.log(`Pull Requests Status: ${pullRequestsResponse.status}`)
 
-//       const rawResponse = await response.text()
+//     // Read the raw response text
+//     const rawCommitsResponse = await commitsResponse.text()
+//     const rawPullRequestsResponse = await pullRequestsResponse.text()
 
-//       if (!response.ok) {
-//         throw new Error(`Error fetching commits: ${response.status}`)
-//       }
-
-//       let pageCommits
-//       try {
-//         pageCommits = JSON.parse(rawResponse)
-//       } catch (parseError) {
-//         throw new Error(
-//           `Failed to parse JSON response: ${(parseError as Error).message}`,
-//         )
-//       }
-
-//       console.log(`Commits: ${JSON.stringify(pageCommits)}`)
-//       commits = [...commits, ...pageCommits]
-
-//       if (pageCommits.length < 100) {
-//         fetchMore = false
-//       } else {
-//         page++
-//       }
+//     if (!commitsResponse.ok) {
+//       throw new Error(`Error fetching commits: ${commitsResponse.status}`)
+//     }
+//     if (!pullRequestsResponse.ok) {
+//       throw new Error(
+//         `Error fetching pull requests: ${pullRequestsResponse.status}`,
+//       )
 //     }
 
-//     // const commits = await response.json()
+//     let commits, pullRequests
+//     try {
+//       commits = JSON.parse(rawCommitsResponse)
+//       pullRequests = JSON.parse(rawPullRequestsResponse)
+//     } catch (parseError) {
+//       throw new Error(
+//         `Failed to parse JSON response: ${(parseError as Error).message}`,
+//       )
+//     }
+
+//     // console.log(`Commits: ${JSON.stringify(commits)}`)
+
 //     const formattedCommits = commits.map((commit: any) => {
 //       const status =
 //         commit.commit.verification && commit.commit.verification.verified
 //           ? 'Verified'
 //           : 'Unverified'
+//       const pullRequest = pullRequests.find(
+//         (pr: any) => pr.head.sha === commit.sha,
+//       )
+//       const pullRequestStatus = pullRequest ? 'Open PR' : 'No PR'
 
 //       return {
 //         commit: commit.commit.message,
@@ -68,10 +81,10 @@
 //         author: commit.commit.author.name,
 //         date: commit.commit.author.date,
 //         status: status,
+//         pullRequestStatus: pullRequestStatus,
 //         actions: [
-//           { name: 'View', url: `${url}/${commit.sha}` },
+//           { name: 'View', url: `${commitsUrl}/${commit.sha}` },
 //           { name: 'Github', url: commit.html_url },
-//           // { name: 'Notebook', url: `http://yourapp.com/notebook/${commit.sha}` },
 //         ] as Action[],
 //         avatar_url: commit.committer
 //           ? commit.committer.avatar_url
@@ -89,6 +102,44 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { Action } from '../../../types/types'
 
+export const fetchCommitsForUserInRepo = async (
+  githubToken: string,
+  orgName: string,
+  repoName: string,
+  page: string,
+  per_page: string,
+) => {
+  const commitsUrl = `https://api.github.com/repos/${orgName}/${repoName}/commits?page=${page}&per_page=${per_page}`
+  const pullRequestsUrl = `https://api.github.com/repos/${orgName}/${repoName}/pulls?state=open`
+
+  const [commitsResponse, pullRequestsResponse] = await Promise.all([
+    fetch(commitsUrl, {
+      headers: {
+        Authorization: `token ${githubToken}`,
+      },
+    }),
+    fetch(pullRequestsUrl, {
+      headers: {
+        Authorization: `token ${githubToken}`,
+      },
+    }),
+  ])
+
+  if (!commitsResponse.ok) {
+    throw new Error(`Error fetching commits: ${commitsResponse.status}`)
+  }
+  if (!pullRequestsResponse.ok) {
+    throw new Error(
+      `Error fetching pull requests: ${pullRequestsResponse.status}`,
+    )
+  }
+
+  const commits = await commitsResponse.json()
+  const pullRequests = await pullRequestsResponse.json()
+
+  return { commits, pullRequests }
+}
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const {
     repoName,
@@ -104,54 +155,16 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
   console.log(`Fetching commits for ${orgName}/${repoName}`)
 
-  const commitsUrl = `https://api.github.com/repos/${orgName}/${repoName}/commits?page=${page}&per_page=${per_page}`
-  const pullRequestsUrl = `https://api.github.com/repos/${orgName}/${repoName}/pulls?state=open`
   const token = process.env.GITHUB_TOKEN
 
-  console.log(`GitHub API URL: ${commitsUrl}`)
-  console.log(`GitHub Token: ${token ? 'Present' : 'Not Present'}`)
-
   try {
-    const [commitsResponse, pullRequestsResponse] = await Promise.all([
-      fetch(commitsUrl, {
-        headers: {
-          Authorization: `token ${token}`,
-        },
-      }),
-      fetch(pullRequestsUrl, {
-        headers: {
-          Authorization: `token ${token}`,
-        },
-      }),
-    ])
-
-    console.log(`Commits Status: ${commitsResponse.status}`)
-    console.log(`Pull Requests Status: ${pullRequestsResponse.status}`)
-
-    // Read the raw response text
-    const rawCommitsResponse = await commitsResponse.text()
-    const rawPullRequestsResponse = await pullRequestsResponse.text()
-
-    if (!commitsResponse.ok) {
-      throw new Error(`Error fetching commits: ${commitsResponse.status}`)
-    }
-    if (!pullRequestsResponse.ok) {
-      throw new Error(
-        `Error fetching pull requests: ${pullRequestsResponse.status}`,
-      )
-    }
-
-    let commits, pullRequests
-    try {
-      commits = JSON.parse(rawCommitsResponse)
-      pullRequests = JSON.parse(rawPullRequestsResponse)
-    } catch (parseError) {
-      throw new Error(
-        `Failed to parse JSON response: ${(parseError as Error).message}`,
-      )
-    }
-
-    // console.log(`Commits: ${JSON.stringify(commits)}`)
+    const { commits, pullRequests } = await fetchCommitsForUserInRepo(
+      token!,
+      orgName,
+      repoName,
+      page,
+      per_page,
+    )
 
     const formattedCommits = commits.map((commit: any) => {
       const status =
@@ -171,7 +184,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         status: status,
         pullRequestStatus: pullRequestStatus,
         actions: [
-          { name: 'View', url: `${commitsUrl}/${commit.sha}` },
+          { name: 'View', url: `${commit.html_url}` },
           { name: 'Github', url: commit.html_url },
         ] as Action[],
         avatar_url: commit.committer
