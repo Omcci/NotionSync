170f745037206718e2bd3927f37fb0e52ed216e9
Omci <melloulchi.omar@gmail.com>
2024-07-11 22:34:12 +0200

    feat: adapt cards for smaller devices

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 
 const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-50 dark:bg-gray-900">
+    <main className="flex min-h-screen flex-col items-center justify-between p-8 md:p-24 bg-gray-50 dark:bg-gray-900">
       <header className="w-full max-w-3xl text-sm font-mono">
         <p className="mb-6 text-4xl font-bold text-gray-800 dark:text-gray-100">
           Welcome to NotionSync
@@ -24,8 +24,8 @@ const Home = () => {
         </div>
       </header>
       <section className="grid grid-cols-1 md:grid-cols-3 gap-8 text-center mt-12">
-        <Card>
-          <Image src="/neural.jpg" alt="Workflow Image" width={400} height={200} className="rounded-t-lg" />
+        <Card className="flex flex-col">
+          <Image src="/neural.jpg" alt="Workflow Image" width={400} height={200} className="rounded-t-lg w-full h-32 object-cover" />
           <CardHeader>
             <CardTitle>Automate Your Workflow</CardTitle>
             <CardDescription>
@@ -38,24 +38,22 @@ const Home = () => {
             </p>
           </CardContent>
         </Card>
-        <Card>
+        <Card className="flex flex-col">
+          <Image src="/collab.jpg" alt="Collaboration Image" width={400} height={200} className="rounded-t-lg w-full h-32 object-cover" />
           <CardHeader>
             <CardTitle>Enhance Collaboration</CardTitle>
             <CardDescription>
               Keep your team on the same page.
             </CardDescription>
           </CardHeader>
-
           <CardContent>
             <p className="text-gray-600 dark:text-gray-300">
               Share real-time updates with your team and enhance collaboration by integrating commit messages and branch updates directly into your Notion pages.
             </p>
           </CardContent>
-          <Image src="/collab.jpg" alt="Collaboration Image" width={400} height={200} className="rounded-b-lg" />
-
         </Card>
-        <Card>
-          <Image src="/gh.webp" alt="Repository Image" width={400} height={200} className="rounded-t-lg" />
+        <Card className="flex flex-col">
+          <Image src="/gh.webp" alt="Repository Image" width={400} height={200} className="rounded-t-lg w-full h-32 object-cover" />
           <CardHeader>
             <CardTitle className="text-2xl font-semibold text-gray-800 dark:text-gray-100">
               GitHub Repository &rarr;
