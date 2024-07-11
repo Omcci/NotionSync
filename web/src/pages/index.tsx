cbb2c5d2ea65c378c843476afd45dcfefaba42ab
Omci <melloulchi.omar@gmail.com>
2024-07-11 22:24:26 +0200

    feat: improve homepage - add img to cards

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,15 +1,20 @@
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import Image from 'next/image'
 import Link from 'next/link'
 
 const Home = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-50 dark:bg-gray-900">
-      <header className="w-full max-w-5xl text-center text-sm font-mono">
+      <header className="w-full max-w-3xl text-sm font-mono">
         <p className="mb-6 text-4xl font-bold text-gray-800 dark:text-gray-100">
           Welcome to NotionSync
         </p>
-        <div className="flex justify-center items-center border-b border-gray-300 dark:border-gray-700 py-4">
+        <p className="mb-8 text-lg text-gray-600">
+          Seamlessly sync your GitHub commits with Notion to keep your project
+          management up-to-date and effortless.
+        </p>
+        <div className="flex  border-b border-gray-300 dark:border-gray-700 py-4">
           <span className="text-gray-600 dark:text-gray-300">
             Get started by going to{' '}
             <code className="font-bold text-gray-900 dark:text-gray-100">
@@ -18,23 +23,39 @@ const Home = () => {
           </span>
         </div>
       </header>
-      <section className="grid grid-cols-1 md:grid-cols-2 gap-4 text-center mt-12">
+      <section className="grid grid-cols-1 md:grid-cols-3 gap-8 text-center mt-12">
         <Card>
+          <Image src="/neural.jpg" alt="Workflow Image" width={400} height={200} className="rounded-t-lg" />
           <CardHeader>
-            <CardTitle className="text-2xl font-semibold text-gray-800 dark:text-gray-100">
-              Documentation &rarr;
-            </CardTitle>
-            <CardDescription className="mt-2 text-base text-gray-600 dark:text-gray-300">
-              Find in-depth information about Next.js features and API.
+            <CardTitle>Automate Your Workflow</CardTitle>
+            <CardDescription>
+              Automatically sync GitHub commits to Notion.
             </CardDescription>
           </CardHeader>
           <CardContent>
-            <Button asChild>
-              <Link href="https://nextjs.org/docs">Learn More</Link>
-            </Button>
+            <p className="text-gray-600 dark:text-gray-300">
+              No more manual updates. Ensure your Notion workspace is always up-to-date with the latest changes from your repositories.
+            </p>
           </CardContent>
         </Card>
         <Card>
+          <CardHeader>
+            <CardTitle>Enhance Collaboration</CardTitle>
+            <CardDescription>
+              Keep your team on the same page.
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent>
+            <p className="text-gray-600 dark:text-gray-300">
+              Share real-time updates with your team and enhance collaboration by integrating commit messages and branch updates directly into your Notion pages.
+            </p>
+          </CardContent>
+          <Image src="/collab.jpg" alt="Collaboration Image" width={400} height={200} className="rounded-b-lg" />
+
+        </Card>
+        <Card>
+          <Image src="/gh.webp" alt="Repository Image" width={400} height={200} className="rounded-t-lg" />
           <CardHeader>
             <CardTitle className="text-2xl font-semibold text-gray-800 dark:text-gray-100">
               GitHub Repository &rarr;
