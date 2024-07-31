261c4e372dfdb1798ba8085bc28037da8a489c6c
Omar MELLOULCHI <omar.melloulchi@greenwishes.fr>
2024-07-31 12:00:43 +0200

    chore: Remove ConfigSettings component from DashboardV0 page

--- web/src/pages/dashboardv0.tsx
+++ web/src/pages/dashboardv0.tsx
@@ -3,17 +3,15 @@ import HeaderV0 from '@/components/HeaderV0'
 import SyncStatus from '@/components/SyncStatus'
 import BranchSelector from '@/components/BranchSelector'
 import CommitLog from '@/components/commit-log/CommitLog'
-import ConfigSettings from '@/components/config-settings/ConfigSettings'
 
 const DashboardV0 = () => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col min-h-screen">
       <HeaderV0 />
       <main className="flex-1 bg-gray-100 dark:bg-gray-800 p-6">
         <SyncStatus />
         <BranchSelector />
         <CommitLog />
-        <ConfigSettings />
       </main>
     </div>
   )
