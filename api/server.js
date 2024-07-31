d76d8d0e215d45e5363e441d556a49897bdb6cea
Omar MELLOULCHI <omar.melloulchi@greenwishes.fr>
2024-07-31 11:46:20 +0200

    chore: remove unused API endpoints and configuration options

--- api/server.js
+++ api/server.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import cors from 'cors'
 import { NotionSync } from './src/notionSync.js'
-import { githubToken, orgName, notionToken, repoName } from './utils/config.js'
 
 const app = express()
 const port = process.env.PORT || 4001
@@ -9,24 +8,6 @@ const notionSync = new NotionSync()
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/config', (req, res) => {
-  res.json({
-    githubToken,
-    notionToken,
-    repoName,
-    orgName,
-  })
-})
-
-app.post('/api/config', (req, res) => {
-  const { githubToken, notionToken, repoName, orgName } = req.body
-  if (!githubToken || !notionToken || !repoName || !orgName) {
-    return res.status(400).json({ error: 'All fields are required' })
-  }
-  config = { repository, organization, githubToken, notionToken }
-  res.json(config)
-})
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
