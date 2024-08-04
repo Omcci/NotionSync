f228915ca6489e4bdecea2239f5659fc2066e26d
Omci <melloulchi.omar@gmail.com>
2024-08-04 22:45:08 +0100

    refactor: change api call to fetch commits in calendar page

--- web/src/pages/calendar.tsx
+++ web/src/pages/calendar.tsx
@@ -3,15 +3,32 @@ import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import { Commit } from '../../types/types';
+import { useRouter } from 'next/router';
 
 const CalendarPage = () => {
   const [events, setEvents] = useState([]);
+  const router = useRouter();
+  const { orgName, repoName } = router.query;
+  console.log('orgName:', orgName);
+  console.log('repoName:', repoName);
 
   useEffect(() => {
+    if (!orgName || !repoName) return;
+
     const fetchCommits = async () => {
+      const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+      const url = `${apiUrl}/api/commits?repoName=${repoName}&orgName=${orgName}`;
+      console.log('fetching sync status', url);
+
       try {
-        const response = await fetch('/api/github/commits');
+        const response = await fetch(url);
+        console.log('response:', response);
+        if (!response.ok) {
+          const errorText = await response.text();
+          throw new Error(`Error fetching commits: ${response.status} - ${errorText}`);
+        }
         const data = await response.json();
+        console.log('data:', data);
         const formattedEvents = data.map((commit: Commit) => ({
           title: commit.commit,
           date: commit.date,
@@ -21,9 +38,8 @@ const CalendarPage = () => {
         console.error('Error fetching commits:', error);
       }
     };
-
     fetchCommits();
-  }, []);
+  }, [repoName, orgName]);
 
   return (
     <div className="container mx-auto p-4">
