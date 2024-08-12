8f0787af062cacd507b442c59243346e16f67e92
Omci <melloulchi.omar@gmail.com>
2024-08-12 21:27:49 +0100

    feat: create Logout button

---
+++ web/src/components/LogoutButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import signOut from '../lib/logout';
+import { Button } from '@/components/ui/button';
+
+const LogoutButton = () => (
+  <Button onClick={signOut} variant="destructive">
+    Log out
+  </Button>
+);
+
+export default LogoutButton;
