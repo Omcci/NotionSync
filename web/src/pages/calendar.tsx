c1e372d4d4ef34c0d2d4df27ced8dcaef530c248
Omci <melloulchi.omar@gmail.com>
2024-08-20 22:51:54 +0200

    chore: remove comments

--- web/src/pages/calendar.tsx
+++ web/src/pages/calendar.tsx
@@ -6,22 +6,12 @@ import { Commit } from '../../types/types'
 import { useRouter } from 'next/router'
 import { useAppContext } from '@/context/AppContext'
 import SelectComponent from '@/components/SelectComponent'
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/ui/dialog'
 import ModalCommits from '@/components/ModalCommits'
 
 //TODO : add mistral to make a summary of the day
 const CalendarPage = () => {
   const [events, setEvents] = useState([])
   const router = useRouter()
-  // const { orgName, repoName } = router.query;
-  // console.log('orgName:', orgName);
-  // console.log('repoName:', repoName);
 
   const [selectedDate, setSelectedDate] = useState('')
   const [commitDetails, setCommitDetails] = useState<Commit[]>([])
@@ -30,29 +20,6 @@ const CalendarPage = () => {
   const { repos, selectedRepo, setSelectedRepo, setRepos } = useAppContext()
   console.log('repos', repos)
   const { org: orgName, name: repoName } = selectedRepo || {}
-
-  // useEffect(() => {
-  //   const fetchRepos = async () => {
-  //     if (repos?.length === 0) {
-  //       const username = process.env.NEXT_PUBLIC_USERNAME;
-  //       const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-  //       const url = `${apiUrl}/api/repos?username=${encodeURIComponent(username!)}`;
-
-  //       try {
-  //         const response = await fetch(url);
-  //         if (!response.ok) {
-  //           throw new Error(`Error fetching repositories: ${response.status}`);
-  //         }
-  //         const data = await response.json();
-  //         setRepos(data.repos);
-  //       } catch (error) {
-  //         console.error('Failed to fetch repositories:', error);
-  //       }
-  //     }
-  //   };
-
-  //   fetchRepos();
-  // }, [repos, setRepos]);
 
   useEffect(() => {
     if (!selectedRepo) return
@@ -129,54 +96,6 @@ const CalendarPage = () => {
           dateClick={handleDateClick}
         />
       </div>
-      {/* <Dialog open={open} onOpenChange={setOpen}>
-        <DialogTrigger asChild>
-          <button className="hidden">Open</button>
-        </DialogTrigger>
-        <DialogContent className="max-w-3xl overflow-y-auto max-h-[75vh]">
-          <DialogHeader>
-            <DialogTitle className="text-xl font-bold">Commits on {selectedDate}</DialogTitle>
-          </DialogHeader>
-          {commitDetails.length === 0 ? (
-            <p className="text-gray-500">No commits found for this date.</p>
-          ) : (
-            <ul className="divide-y divide-gray-200">
-              {commitDetails.map((commit, idx) => (
-                <li key={idx} className="p-4">
-                  <div className="flex items-start space-x-4">
-                    <img
-                      src={commit.authorDetails.avatar_url}
-                      alt={`${commit.author}'s avatar`}
-                      className="w-10 h-10 rounded-full"
-                    />
-                    <div>
-                      <p className="text-lg font-semibold">{commit.commit}</p>
-                      <p className="text-gray-500">Author: {commit.author}</p>
-                      <p className="text-gray-500">Date: {new Date(commit.date).toLocaleString()}</p>
-                      <p className="text-gray-500">Status: {commit.status}</p>
-                      <div className="mt-2">
-                        {commit.actions.map((action, actionIdx) => (
-                          <a
-                            key={actionIdx}
-                            href={action.url}
-                            className="text-blue-500 hover:underline mr-4"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                          >
-                            {action.name}
-                          </a>
-                        ))}
-                      </div>
-                    </div>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          )}
-        </DialogContent>
-
-      </Dialog> */}
-
       <ModalCommits
         open={open}
         setOpen={setOpen}
