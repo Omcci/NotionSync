8e97bc0c56872d439f58bbb75997c9d1d665c0e0
Omar MELLOULCHI <omar.melloulchi@greenwishes.fr>
2024-09-13 23:57:34 +0200

    Remove fetch-and-deploy-main.sh from version control

--- fetch-and-deploy-main.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# fetch-and-deploy-main.sh
-docker compose -f docker-compose.prod.yml down --rmi all && \
-    git pull origin main && \
-    GATEWAY_PORT=3000 docker compose -f docker-compose.prod.yml up -d;
