c18ca46fdcd4f5270bdb5148836d8877e6fdf60d
Omar MELLOULCHI <omar.melloulchi@greenwishes.fr>
2024-09-17 11:14:33 +0200

    chore: Update ESLint configuration to use latest ECMAScript version, module syntax, and TypeScript parser

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,19 @@
-module.exports = [
+export default [
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: '@typescript-eslint/parser',
     },
+    plugins: ['@typescript-eslint', 'react', 'jsx-a11y', 'react-hooks'],
     rules: {
       'semi': ['error', 'never'],
       'quotes': ['error', 'single'],
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-unused-vars': 'error',
+      'react/no-unescaped-entities': 'off',
     },
   },
 ];
