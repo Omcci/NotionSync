31d5514df762da53f6ce5963cfb8eab55b0bb673
Omar MELLOULCHI <omar.melloulchi@greenwishes.fr>
2024-09-17 11:21:05 +0200

    chore: Update ESLint configuration to use latest ECMAScript version, module syntax, and TypeScript parser

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,24 @@
-module.exports = {
-  parser: '@typescript-eslint/parser', 
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module', 
+module.exports = [
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: require('@typescript-eslint/parser'),
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+      react: require('eslint-plugin-react'),
+      'jsx-a11y': require('eslint-plugin-jsx-a11y'),
+      'react-hooks': require('eslint-plugin-react-hooks'),
+    },
+    rules: {
+      semi: ['error', 'never'],
+      quotes: ['error', 'single'],
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-unused-vars': 'error',
+      'react/no-unescaped-entities': 'off',
+    },
   },
-  plugins: ['@typescript-eslint', 'react', 'jsx-a11y', 'react-hooks'],
-  rules: {
-    semi: ['error', 'never'],
-    quotes: ['error', 'single'],
-    'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off',
-    '@typescript-eslint/no-unused-vars': 'error',
-    'react/no-unescaped-entities': 'off',
-  },
-}
+]
