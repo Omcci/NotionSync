dea693e58798c808109686481e09c429b33cdecd
Omci <melloulchi.omar@gmail.com>
2024-09-22 12:19:02 +0200

    refactor: update usercontext with major changes
    - use reactquery to handle user auth
    - remove refreshsession for now
    - remove onauthstatechange that's handled by react-query
    - add signout method

--- web/src/context/UserContext.tsx
+++ web/src/context/UserContext.tsx
@@ -1,80 +1,49 @@
 import React, { createContext, useContext, useEffect, useState } from 'react'
 import { supabase } from '@/lib/supabaseClient'
 import { User } from '@supabase/supabase-js'
+import { useQuery } from '@tanstack/react-query'
+import { signOut } from '@/lib/logout'
 
 interface UserContextType {
   user: User | null
   githubToken: string | null
-  refreshSession: () => Promise<void>
+  isLoading: boolean
+  signOutUser: () => Promise<void>
 }
 
 const UserContext = createContext<UserContextType | undefined>(undefined)
 
-export const UserProvider: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => {
+export const UserProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [user, setUser] = useState<User | null>(null)
   const [githubToken, setGithubToken] = useState<string | null>(null)
 
-  const refreshSession = async () => {
-    const { data, error } = await supabase.auth.refreshSession()
-    if (error) {
-      console.error('Error refreshing session:', error.message)
-      await supabase.auth.signOut()
+  const { data, isLoading } = useQuery({
+    queryKey: ['session'],
+    queryFn: async () => {
+      const { data } = await supabase.auth.getSession()
+      return data?.session
+    },
+    staleTime: 1000 * 60 * 5,
+
+  })
+
+  useEffect(() => {
+    if (data) {
+      setUser(data.user)
+      setGithubToken(data?.provider_token ?? null)
+    }
+  }, [data])
+
+  const signOutUser = async () => {
+    const error = await signOut()
+    if (!error) {
       setUser(null)
       setGithubToken(null)
-      localStorage.clear()
-      sessionStorage.clear()
-    } else if (data.session) {
-      setUser(data.session.user)
-      const savedToken = sessionStorage.getItem('github_token')
-      if (!savedToken) {
-        sessionStorage.setItem(
-          'github_token',
-          data.session.provider_token ?? '',
-        )
-        setGithubToken(data.session.provider_token ?? null)
-      } else {
-        setGithubToken(savedToken)
-      }
     }
   }
 
-  useEffect(() => {
-    const getSession = async () => {
-      const { data } = await supabase.auth.getSession()
-      const savedToken = sessionStorage.getItem('github_token')
-      if (savedToken) {
-        setGithubToken(savedToken)
-      } else {
-        setUser(data?.session?.user ?? null)
-        setGithubToken(data?.session?.provider_token ?? null)
-      }
-    }
-
-    getSession()
-
-    const { data: authListener } = supabase.auth.onAuthStateChange(
-      (event, session) => {
-        if (event === 'SIGNED_IN' && session) {
-          setUser(session.user)
-          sessionStorage.setItem('github_token', session.provider_token ?? '')
-          setGithubToken(session.provider_token ?? null)
-        } else if (event === 'SIGNED_OUT') {
-          setUser(null)
-          setGithubToken(null)
-          sessionStorage.clear()
-        }
-      },
-    )
-
-    return () => {
-      authListener.subscription.unsubscribe()
-    }
-  }, [])
-
   return (
-    <UserContext.Provider value={{ user, githubToken, refreshSession }}>
+    <UserContext.Provider value={{ user, githubToken, isLoading, signOutUser }}>
       {children}
     </UserContext.Provider>
   )
