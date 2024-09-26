16796ef0ed964c6c4d2afa8aec6bcedda3f8880f
Omci <melloulchi.omar@gmail.com>
2024-09-26 21:03:54 +0200

    chore: hide prompt

--- web/src/pages/api/mistral.ts
+++ web/src/pages/api/mistral.ts
@@ -21,28 +21,12 @@ export default async function handler(
   const combinedCommitMessage = commits.map((c) => c.commitMessage).join('\n\n')
   const combinedDiff = commits.map((c) => c.diff).join('\n\n')
 
-  const prompt = `
-    Based on the series of commits and code changes below, provide a cohesive summary in HTML format.
-    Please ensure:
-    - Use sentence case for headings (capitalize only the first word in titles).
-    - Avoid using uppercase for every word in titles.
-    - Provide the output directly in HTML format.
-    - Use the following Tailwind CSS classes for headings:
-        - <h1 class="text-3xl font-bold"> for the main heading
-        - <h2 class="text-2xl font-semibold"> for subheadings
-        - <h3 class="text-xl font-medium"> for smaller sections
-        - <p class="text-gray-600> for paragraphs
-        - <li class="text-gray-600"> for list items
-    - Use <ul class="list-disc ml-6"> for bullet points and <strong> to bold key points.
-    
-    Summary should include:
-    - The overall purpose of the changes
-    - Key code changes, such as additions, deletions, or modifications
-    - Logic improvements, bug fixes, or optimizations made to the application
-    - Impact on performance and user experience
-    Commit Message: ${combinedCommitMessage}
-    Code Changes: ${combinedDiff}
-  `
+  const promptTemplate =
+    process.env.NEXT_PUBLIC_MISTRAL_PROMPT || 'Default prompt content'
+
+  const prompt = promptTemplate
+    ?.replace('{COMBINED_COMMIT_MESSAGE}', combinedCommitMessage)
+    .replace('{COMBINED_DIFF}', combinedDiff)
   try {
     const chatResponse = await client.chat({
       model: 'mistral-small-latest',
