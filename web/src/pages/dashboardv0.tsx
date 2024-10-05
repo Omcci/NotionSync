997de5d6fb673f1368af7a61f6dc6d980a92a0e0
Omci <melloulchi.omar@gmail.com>
2024-10-05 10:16:40 +0200

    style: fix responsiveness in dashboardv0

--- web/src/pages/dashboardv0.tsx
+++ web/src/pages/dashboardv0.tsx
@@ -4,10 +4,9 @@ import SyncStatus from '@/components/SyncStatus'
 import BranchSelector from '@/components/BranchSelector'
 import CommitLog from '@/components/commit-log/CommitLog'
 
-// TODO : Move the github login in header outside of the dashboard
 const DashboardV0 = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col sm:container">
       <HeaderV0 />
       <div className="px-4">
         <h1 className="text-2xl font-bold ">Dashboard.</h1>
