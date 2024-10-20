c39f5931d8c43088497f9ef97337a884d883d885
Omci <melloulchi.omar@gmail.com>
2024-10-20 21:45:41 +0200

    fix: add ref to profilebutton component

--- web/src/components/ProfileButton.tsx
+++ web/src/components/ProfileButton.tsx
@@ -1,9 +1,8 @@
 import { useRouter } from 'next/router'
 import { UserIcon } from '../../public/icon/UserIcon'
+import React from 'react'
 
-// query parameter to add
-
-const ProfileButton = () => {
+const ProfileButton = React.forwardRef<HTMLButtonElement, React.ButtonHTMLAttributes<HTMLButtonElement>>((props, ref) => {
   const router = useRouter()
   const handleShowProfile = () => {
     router.push('/profile')
@@ -12,12 +11,16 @@ const ProfileButton = () => {
   return (
     <button
       onClick={handleShowProfile}
-      className="flex items-center w-full px-4 py-2 text-left text-gray-700 dark:text-gray-300 hover:bg-gray-200 dark:hover:bg-gray-800"
+      ref={ref}
+      className={`flex items-center w-full px-4 py-2 text-left text-gray-700 dark:text-gray-300 hover:bg-gray-200 dark:hover:bg-gray-800 ${props.className}`}
+      {...props}
     >
       <UserIcon className="h-5 mr-2" />
       Profile
     </button>
   )
-}
+})
+
+ProfileButton.displayName = 'ProfileButton'
 
 export default ProfileButton
