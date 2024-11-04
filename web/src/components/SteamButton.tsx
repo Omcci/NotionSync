b22e436b96cef5de3b659f3d0d7aafac979cd668
Omci <melloulchi.omar@gmail.com>
2024-11-04 20:41:21 +0100

    style: update title / button cta for light mode

--- web/src/components/SteamButton.tsx
+++ web/src/components/SteamButton.tsx
@@ -13,10 +13,9 @@ const SteamButton = ({ children, onClick }: GlowingButtonProps) => {
 
   return (
     <motion.button
-      whileHover={{ scale: 1.01 }}
       whileTap={{ scale: 0.98 }}
       className={`relative inline-flex items-center justify-center px-6 py-3 font-semibold rounded-[calc(0.375rem+4px)] shadow-lg overflow-hidden 
-        ${isDarkMode ? 'text-white bg-blue-200' : 'text-gray-800 bg-blue-200'}
+        ${isDarkMode ? 'text-white bg-blue-200' : 'text-gray-800 bg-white'}
       `}
       onClick={onClick}
     >
@@ -27,12 +26,12 @@ const SteamButton = ({ children, onClick }: GlowingButtonProps) => {
       ></span>
       <span
         className={`absolute inset-0 w-full h-full rounded-[calc(0.375rem+4px)] bg-[length:400%] animate-steam blur-lg
-          ${isDarkMode ? 'bg-gradient-to-r from-white via-gray-200 to-gray-500' : 'bg-gradient-to-r from-blue-300 via-blue-400 to-blue-500'}
+          ${isDarkMode ? 'bg-gradient-to-r from-white via-gray-200 to-gray-500' : 'bg-gradient-to-r from-gray-950 via-gray-800 to-gray-600'}
         `}
       ></span>
       <span
         className={`absolute inset-0.5 rounded-lg 
-        bg-blue-300
+        dark:bg-blue-300 bg-white
         `}
       ></span>
       <span
